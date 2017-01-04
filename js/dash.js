2edff6ff611e0466d910d7b322ec115e55a5fada
LuoJieLiang <darwin7381987654@gmail.com>
2017-01-05 03:53:41 +0800

    cool

--- js/dash.js
+++ js/dash.js
@@ -1,3 +1,30 @@
+$(".like").click(function () {
+    $(".like").html("<img class='image' src='assets/red_heart.png' height='30' width='30'>")
+})
+
+$(".arrow").click(function(){
+    var x = 60
+        d = $(".arrow").width()/2
+
+    if ($(".story").width()/$(window).width() < 0.5) {
+        var w = $(".story").width() + $(window).width()*x/100
+
+        $(".story").css("width",w);
+        $(".story").css('background-color','rgba(0,0,0,.8)');
+        $(".arrow").css("left",w-d);
+        $(".arrow").html("<")  
+
+    }else{
+
+        var w = $(".story").width() - $(window).width()*x/100
+        $(".story").css("width","");
+        $(".story").css('background-color',"");
+        $(".arrow").css("left","");
+        $(".arrow").html(">") 
+    }
+})
+
+
 $(function () {
 
     function initMap() {
@@ -40,8 +67,6 @@ $(function () {
         var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
 
         map.set('styles', styles);
-
-
     }
 
     google.maps.event.addDomListener(window, 'load', initMap);
